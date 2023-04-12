9358e1c55ad2ed92b187a344beab82656757a6b8
jall0101 <92348560+jall0101@users.noreply.github.com>
2023-04-12 14:34:38 -0600

    Cambios en caballeros

--- src/Controllers/clientView/Caballeros.php
+++ src/Controllers/clientView/Caballeros.php
@@ -8,10 +8,28 @@ use Views\Renderer;
 
 class Caballeros extends PublicController {
    
+    private $viewData = array();
     public function run() :void
     {
-        $viewData = array();        
-        Renderer::render('clientView/caballeros', $viewData);
+           
+        $this->page_loaded();
+        $this->viewData["cantidad"] = count($this->viewData["caballeros"]);
+        $this->viewData["marcas"] = \Dao\Mnt\Marcas::findAll();  
+        Renderer::render('clientView/caballeros', $this->viewData);
+    }
+
+    private function page_loaded()
+    {
+        if (isset($_GET['marcacod'])) {
+            $this->viewData["caballeros"] = \Dao\Mnt\Zapatos::findByBrand($_GET['marcacod'],1);
+        }else if(isset($_GET['nombre'])){
+            $this->viewData["caballeros"] = \Dao\Mnt\Zapatos::findByName($_GET['nombre'],1);
+        }
+         else {
+            $this->viewData["caballeros"] = \Dao\Mnt\Zapatos::findByDepartment(1);
+        }
+
+
     }
 }
 ?>
